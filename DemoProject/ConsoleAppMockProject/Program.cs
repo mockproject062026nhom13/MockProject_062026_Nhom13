cee10952874df70b697a03cc8eb979fa7afc7668
BuiXuanVan294 <22115053122147@sv.ute.udn.vn>
2026-06-29 10:11:51 +0700

    Văn_Bài toán cộng để test pull request

--- DemoProject/ConsoleAppMockProject/Program.cs
+++ DemoProject/ConsoleAppMockProject/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int a = 5;
+int b = 10;
+int tong = a + b;
+
+Console.WriteLine($"Kết quả của phép cộng {a} + {b} là: {tong}");
