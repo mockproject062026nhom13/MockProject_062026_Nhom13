1b2d9bef72539f145601b5cf3123d101508184b5
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-06-29 10:23:40 +0700

    thực hiện phép chia

--- DemoProject/ConsoleAppMockProject/Program.cs
+++ DemoProject/ConsoleAppMockProject/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int a = 10;
+int b = 5;
+Console.WriteLine($"The quotient of {a} and {b} is {a / b}");
