92ffd7bfc1a6231e4da141546b03f13aff9ca6ff
Long <longs3515@gmail.com>
2026-06-29 10:10:54 +0700

    test tru

--- DemoProject/ConsoleAppMockProject/Program.cs
+++ DemoProject/ConsoleAppMockProject/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.Write("Nhập số thứ nhất: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Nhập số thứ hai: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+double result = num1 - num2;
+
+Console.WriteLine($"Kết quả của {num1} - {num2} = {result}");
