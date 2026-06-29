e0a83600e502909c9ddddfa5cf8405e7a4c2ab5f
ngoduonghoangchau <chau78840@gmail.com>
2026-06-29 09:49:30 +0700

    Mod Operator

--- DemoProject/ConsoleAppMockProject/Program.cs
+++ DemoProject/ConsoleAppMockProject/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine($"17 mod 4 = {ModOperator(17, 4)}");
+return;
+
+static int ModOperator(int a, int b)
+{
+    return a % b;
+}
