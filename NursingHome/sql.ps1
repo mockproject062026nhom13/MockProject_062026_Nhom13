fff9cba0bb1d14e7428a4467df3c794751d31dd3
HuyHiHung <126769158+HuyHiHung@users.noreply.github.com>
2026-07-06 12:48:13 +0700

    add script sql.ps1 to quickly into database by using .\sql.ps1

---
+++ NursingHome/sql.ps1
@@ -0,0 +1,15 @@
+# Đọc file .env
+$envFile = ".env"
+
+$env:SA_PASSWORD = (
+    Get-Content .env |
+    Where-Object { $_ -match '^MSSQL_SA_PASSWORD=' } |
+    ForEach-Object { ($_ -split '=',2)[1] }
+)
+
+docker exec -it nursinghome-db `
+    /opt/mssql-tools18/bin/sqlcmd `
+    -S localhost `
+    -U sa `
+    -P $env:SA_PASSWORD `
+    -C
