13eade36d7eec253830c449b3ad5c97bfcc1dfeb
HuyHiHung <126769158+HuyHiHung@users.noreply.github.com>
2026-07-06 13:12:21 +0700

    fixing sql.ps1 direct to NursingHome

--- NursingHome/sql.ps1
+++ NursingHome/sql.ps1
@@ -2,7 +2,7 @@
 $envFile = ".env"
 
 $env:SA_PASSWORD = (
-    Get-Content .env |
+    Get-Content $envFile |
     Where-Object { $_ -match '^MSSQL_SA_PASSWORD=' } |
     ForEach-Object { ($_ -split '=',2)[1] }
 )
@@ -12,4 +12,5 @@ docker exec -it nursinghome-db `
     -S localhost `
     -U sa `
     -P $env:SA_PASSWORD `
+    -d NursingHome `
     -C
