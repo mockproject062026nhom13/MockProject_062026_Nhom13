4bd9504fc437e9bb3125140dd4d82babce1636b5
Long <longs3515@gmail.com>
2026-07-07 14:40:03 +0700

    handle api getbed with Apiresponse

---
+++ NursingHome/src/NursingHome.Domain/Constants/RoomConstants.cs
@@ -0,0 +1,11 @@
+namespace NursingHome.Domain.Constant;
+
+public static class RoomConstants
+{
+    public static readonly List<string> RoomTypes = new()
+    {
+        "private",
+        "semi-private",
+        "ward"
+    };
+}
