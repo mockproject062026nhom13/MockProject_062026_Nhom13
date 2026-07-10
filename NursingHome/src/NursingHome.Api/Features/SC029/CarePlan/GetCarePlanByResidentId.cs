2179845efb5c63f064165fb3b9fbe15a16dfb2e6
Long <longs3515@gmail.com>
2026-07-10 21:19:25 +0700

    add Dapper

--- NursingHome/src/NursingHome.Api/Features/SC029/CarePlan/GetCarePlanByResidentId.cs
+++ NursingHome/src/NursingHome.Api/Features/SC029/CarePlan/GetCarePlanByResidentId.cs
@@ -2,6 +2,7 @@ using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
 using NursingHome.Application.Common;
 using NursingHome.Infrastructure.Persistence.DbContexts;
+using Dapper;
 
 
 namespace NursingHome.Api.Features.CarePlan;
@@ -15,12 +16,13 @@ public class GetCarePlanEndpoint(NursingHomeDbContext db)
 {
     public override void Configure()
     {
-        Get("/api/care-plan");
+        Get("/api/care-plan/{ResidentId}/{FacilityId}");
         AllowAnonymous();
     }
 
     public override async Task HandleAsync(GetRequest req, CancellationToken ct)
     {
+        var connection = db.Database.GetDbConnection();
         var data = new List<CarePlanDto>();
         var response = ApiResponse<List<CarePlanDto>>.CreateSuccess(data);
         await SendAsync(response, 200, ct);
